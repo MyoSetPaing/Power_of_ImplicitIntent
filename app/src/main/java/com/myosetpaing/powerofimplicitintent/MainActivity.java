30a7765a3a6ce7afcad88bcc7f26b8f942a7aa3c
Myo Set Paing <myosetpaing@aceplussolutions.com>
2019-02-23 09:08:25 +0630

    Implememt power of implicit intent

--- app/src/main/java/com/myosetpaing/powerofimplicitintent/MainActivity.java
+++ app/src/main/java/com/myosetpaing/powerofimplicitintent/MainActivity.java
@@ -1,13 +1,128 @@
 package com.myosetpaing.powerofimplicitintent;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.AlarmClock;
+import android.provider.CalendarContract;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.chip.Chip;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.VideoView;
+
+import java.time.Clock;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_TIME_GET = 1;
+    private static final int REQUEST_CALENDER_EVENT = 2;
+    private static final int REQUEST_VIDEO_CAPTURE = 3;
+    private static final int REQUEST_SELECT_PHONE_NUMBER = 4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btnTimer = findViewById(R.id.btn_timer);
+        btnTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER);
+                startActivityForResult(intent, REQUEST_TIME_GET);
+
+            }
+        });
+        Button btnCalender = findViewById(R.id.btn_calender_event);
+        btnCalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI);
+                startActivityForResult(intent, REQUEST_CALENDER_EVENT);
+            }
+        });
+        Button btnVideoCapture = findViewById(R.id.btn_video_capture);
+        btnVideoCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+                }
+
+            }
+        });
+        Button btnContact = findViewById(R.id.btn_contact);
+        btnContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_SELECT_PHONE_NUMBER);
+                }
+            }
+        });
+
+        Button btnWebSearch = findViewById(R.id.btn_search);
+        btnWebSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextInputEditText textInputEditText = findViewById(R.id.tiet_query);
+                String query = textInputEditText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, query);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        VideoView videoView = findViewById(R.id.videoView);
+        Chip phno = findViewById(R.id.cp_phone_number);
+
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            videoView.setVisibility(View.VISIBLE);
+            phno.setVisibility(View.GONE);
+            Uri videoUri = data.getData();
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+        } else if (requestCode == REQUEST_SELECT_PHONE_NUMBER && resultCode == RESULT_OK) {
+            phno.setVisibility(View.VISIBLE);
+            videoView.setVisibility(View.GONE);
+
+            // Get the URI and query the content provider for the phone number
+            Uri contactUri = data.getData();
+            String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
+            Cursor cursor = getContentResolver().query(contactUri, projection,
+                    null, null, null);
+            // If the cursor returned is valid, get the phone number
+            if (cursor != null && cursor.moveToFirst()) {
+                int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(numberIndex);
+                phno.setText(number);
+            }
+        }
+
+    }
+
 }
